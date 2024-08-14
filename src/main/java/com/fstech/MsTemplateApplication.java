a863000b114292d24d14939a99f295f01e7b7180
David Corredor Ramírez <dgcorredorr@gmail.com>
2024-08-14 01:29:44 -0500

    feat: :sparkles: FST-19 añadir nueva dependencia lib-rest-spring | ajustar manejo de tareas y mensajes

--- src/main/java/com/fstech/MsTemplateApplication.java
+++ src/main/java/com/fstech/MsTemplateApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.fstech.common.configuration.GeneralConfig;
 import com.fstech.common.utils.log.ServiceLogger;
 import com.fstech.common.utils.enums.LogLevel;
-import com.fstech.common.utils.enums.Task;
 
 @SpringBootApplication
 public class MsTemplateApplication {
@@ -18,15 +17,10 @@ public class MsTemplateApplication {
 		SpringApplication.run(MsTemplateApplication.class, args);
 		long endTime = System.currentTimeMillis();
 		long executionTime = endTime - startTime;
-		Task task = Task.INIT_MICROSERVICE;
-		task.setOrigin(Task.Origin.builder()
-				.originClass(MsTemplateApplication.class.getName())
-				.originMethod("main(String[] args)")
-				.build());
 		String logMessage = GeneralConfig.getAppName() + " | " 
 		+ GeneralConfig.getAppVersion() + " | " 
 		+ GeneralConfig.getAppDescription();
-		MsTemplateApplication.logger.log(logMessage, task, LogLevel.INFO, null, executionTime);
+		MsTemplateApplication.logger.log(logMessage, null, LogLevel.INFO, null, executionTime);
 	}
 
 }
--- src/main/java/com/fstech/application/controller/MockController.java
+++ src/main/java/com/fstech/application/controller/MockController.java
@@ -2,16 +2,20 @@ package com.fstech.application.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.fstech.application.dto.GenericRequestDto;
 import com.fstech.application.dto.GenericResponseDto;
+import com.fstech.application.dto.MockDto;
 import com.fstech.application.service.MessageService;
 import com.fstech.common.utils.enums.MessageMapping;
+import com.fstech.provider.RestProvider;
 
 import jakarta.validation.Valid;
 import reactor.core.publisher.Mono;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -25,39 +29,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("api/v1/mock")
 public class MockController {
 
-    private final MessageService messageService;
+        private final MessageService messageService;
+        private final RestProvider restProvider;
 
-    public MockController(MessageService messageService) {
-        this.messageService = messageService;
-    }
+        public MockController(MessageService messageService, RestProvider restProvider) {
+                this.messageService = messageService;
+                this.restProvider = restProvider;
+        }
 
-    @GetMapping
-    public Mono<ResponseEntity<GenericResponseDto>> getMock(ServerWebExchange exchange, @RequestParam String testParam1, @RequestParam(required = false) String testParam2) {
-        return messageService.mapMessage(MessageMapping.DEFAULT_SUCCESS)
-                .map(message -> GenericResponseDto.builder()
-                        .origin(exchange.getRequest().getPath().toString())
-                        .requestId(exchange.getLogPrefix())
-                        .message(message)
-                        .success(true)
-                        .documents(String.join(" ", testParam1, testParam2))
-                        .build())
-                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
-    }
+        @GetMapping
+        public Mono<ResponseEntity<GenericResponseDto>> getMock(ServerWebExchange exchange,
+                        @RequestParam Integer testParam) {
 
-    @PostMapping
-    public Mono<ResponseEntity<GenericResponseDto>> postMock(ServerWebExchange exchange,
-            @Valid @RequestBody Mono<GenericRequestDto> requestDtoMono) {
+                return restProvider.sendRequest(HttpMethod.GET, "https://jsonplaceholder.typicode.com/todos/" + testParam, null,
+                                MockDto.class, null, null)
+                                .flatMap((ResponseEntity<MockDto> todoItem) -> {
+                                        GenericResponseDto genericResponseDto = GenericResponseDto.builder()
+                                                        .origin(exchange.getRequest().getPath().toString())
+                                                        .requestId(exchange.getLogPrefix())
+                                                        .message(messageService
+                                                                        .mapMessage(MessageMapping.DEFAULT_SUCCESS))
+                                                        .success(true)
+                                                        .documents(todoItem.getBody())
+                                                        .build();
+                                        return Mono.just(new ResponseEntity<>(genericResponseDto, HttpStatus.OK));
+                                });
+        }
 
-        return requestDtoMono
-                .flatMap(requestDto -> messageService.mapMessage(MessageMapping.DEFAULT_SUCCESS)
-                        .map(message -> GenericResponseDto.builder()
+        @PostMapping
+        public Mono<ResponseEntity<GenericResponseDto>> postMock(ServerWebExchange exchange,
+                        @Valid @RequestBody GenericRequestDto requestDto) {
+
+                String message = messageService.mapMessage(MessageMapping.DEFAULT_SUCCESS);
+
+                GenericResponseDto response = GenericResponseDto.builder()
                                 .origin(exchange.getRequest().getPath().toString())
                                 .requestId(exchange.getLogPrefix())
                                 .message(message)
                                 .success(true)
                                 .documents(requestDto.getDocuments())
-                                .build())
-                        .map(response -> new ResponseEntity<>(response, HttpStatus.OK)));
-    }
+                                .build();
+
+                return Mono.just(new ResponseEntity<>(response, HttpStatus.OK));
+
+        }
 
 }
